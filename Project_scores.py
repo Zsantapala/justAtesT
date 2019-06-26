270a23b20dca5adbbf996459cd30f501762cf958
samuelZhao <zsqabingdon@vip.qq.com>
2019-06-26 15:13:58 +0800

    improve indentation

--- Project_scores.py
+++ Project_scores.py
@@ -12,14 +12,18 @@ with open('report.txt',encoding='utf-8') as f:
 
 #添加每位同学总成绩和平均成绩
 for i in range(1,len(report)):
-	report[i].append(sum(map(int,report[i][1:])))                             #运用map函数把每位同学每项成绩换成int类型的列表，再由sum函数算总分，最后添加到子列表(同学)中
-	report[i].append(round(int(report[i][-1])/len(report[i][1:-1]),1))              #前面生成的总分与课程数相除得到平均分，round函数只保留小数点前面数字
+	# 运用map函数把每位同学每项成绩换成int类型的列表，再由sum函数算总分，最后添加到子列表(同学)中
+	report[i].append(sum(map(int,report[i][1:])))
+	# 前面生成的总分与课程数相除得到平均分，round函数只保留小数点前面数字
+	report[i].append(round(int(report[i][-1])/len(report[i][1:-1]),1))
 
 #添加平均值
 avg_value=['平均']
+
 #添加每门课平均数
 avg_value=avg_value+[round(sum(map(lambda x:int(x[avg]),report[1:]))/len(report[1:])) for avg in range(1,len(report[1])-1)]
-#写的有点复杂，先用列表生成式提取文本列表的游标(index)值，再用lambda函数提取已avg为引索的int值，再由map生成每门成绩列表，然后由sum求和，再除以学生数，最后round去掉小数点	
+#写的有点复杂，先用列表生成式提取文本列表的游标(index)值，再用lambda函数提取已avg为引索的int值，再由map生成每门成绩列表，然后由sum求和，再除以学生数，最后round去掉小数点
+
 avg_value.append(round(sum(avg_value[1:-1])/9,1))                             #总平均数
 
 #修改report总列表
