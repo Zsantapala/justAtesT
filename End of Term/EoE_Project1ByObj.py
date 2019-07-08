5a56bc9d12e2439ea62f7fc5e716a3dc7e0a09ea
Zsanta Zhao <598194494@qq.com>
2019-07-08 21:09:18 +0800

    improve Object in EOE_OOB thread

--- End of Term/EoE_Project1ByObj.py
+++ End of Term/EoE_Project1ByObj.py
@@ -1,6 +1,5 @@
 #-*-coding:utf-8-*-
 #!/usr/bin/python
-#'https://www.qiushibaike.com/imgrank/'
 import requests,re,threading,os
 from bs4 import BeautifulSoup
 
@@ -36,62 +35,62 @@ class CrawlWeb(object):
         return last_page
 
    #从save_address得到图片地址列表分别打开并保存图片
-    def save_img(self,img_arrd_list):
+    def save_img(self, img):
         try:
-            for img in img_arrd_list:
-                print ('开始下载%s' %img)
-                url = 'http:' + img
-                pic = requests.get(url).content
-                name = 'img//' + re.findall(r'\b\w+.jpg\b|\b\w+.gif', img)[0]
-                with open(name, 'wb') as f:
-                    f.write(pic)
-                print ('下载结束')
+            print ('开始下载%s' %img)
+            url = 'http:' + img
+            pic = requests.get(url).content
+            name = 'img//' + re.findall(r'\b\w+.jpg\b|\b\w+.gif', img)[0]
+            with open(name, 'wb') as f:
+                f.write(pic)
+            print('%s下载结束' %img)
         except :
-            print ('save_img()Error')
+            print('save_img()Error')
 
-
-    def save_address(self,start,end):
-        if start==end:
-            print ('正在下载第%d页的图片' %start)
+    def open_address(self,num):
+        web = (self.page + self.sign1 + '%d' + self.sign2) %num
+        try:
+            get_w = requests.get(web).text
+        except:
+            print('save_address() get_w error')
+        soup = BeautifulSoup(get_w, 'lxml')
+        find_img = self.regula2(soup)
+        if find_img:
+            img_arrd_list = [x['src'] for x in find_img]
         else:
-            print ('正在下载第%d页--%d页的图片' %(start,end))
-        for i in range(start,end+1):
-            print ('下载第%d页图片' % i)
-            web = (self.page + self.sign1 +'%d'+self.sign2) %i
-            try:
-                get_w = requests.get(web).text
-            except :
-                print ('save_address() get_w error')
-            soup = BeautifulSoup(get_w, 'lxml')
-            find_img = self.regula2(soup)
-            if find_img:
-                img_arrd_list = [x['src'] for x in find_img]
-            else:
-                print ('can\'t get data in %s' % web)
-                return
-            # 生成新的子线程来加快下载图片速度
-            son_thread = threading.Thread(target=self.save_img, args=(img_arrd_list,))
+            print('can\'t get data in %s' % web)
+            return
+        # 生成新的子线程来加快下载图片速度
+        son_img = []
+        print('下载第%d页图片' % num)
+        for img in range(len(img_arrd_list)):
+            son_thread = threading.Thread(target=self.save_img, args=(img_arrd_list[img],))
+            #开始运行多线程
             son_thread.start()
+            #把线程对象加入列表
+            son_img.append(son_thread)
+        #并发阻塞直到线程完毕
+        for son_thread in son_img:
             son_thread.join()
-            print ('第%d页下载结束' % i)
+        print('第%d页下载结束' % num)
 
 class QiuShi(CrawlWeb):
     #自定义Qiushi的规则
-    def regula1(self,web1):
+    def regula1(self, web1):
         page = web1.find_all('ul', attrs={"class": "pagination"})[0].find_all('a', attrs={"rel": "nofollow"})
         self.regu1=re.findall(r'\d+', page[-2].span.string)[0]
         return self.regu1
-    def regula2(self,web2):
+    def regula2(self, web2):
         self.regu2 = web2.find_all('img', attrs={"class": "illustration"})
         return self.regu2
 
 class JianDan(CrawlWeb):
     # 自定义JianDan的规则
-    def regula1(self,web1):
+    def regula1(self, web1):
         page = web1.find_all('span', attrs={"class":"current-comment-page"})[0]
-        self.regu1 = re.findall(r'\d',page.text)[0]
+        self.regu1 = re.findall(r'\d', page.text)[0]
         return self.regu1
-    def regula2(self,web2):
+    def regula2(self, web2):
         self.regu2 = web2.find_all('img', attrs={"referrerpolicy": "no-referrer"})
         return self.regu2
 
@@ -101,46 +100,54 @@ def correct_input(max_n):
         while True:
             start_p = input('请输入开始页')
             end_p = input('请输入结束页')
-            try :
-                start_num=int(start_p)
-                end_num=int(end_p)
+            try:
+                start_num = int(start_p)
+                end_num = int(end_p)
                 break
             except ValueError:
                 print('请输入小于等于%d的数字....' %max_n)
                 continue
-        if start_num>max_n or end_num>max_n:
-            print ('本站最多只有%d页,请重新输入' %max_n)
+        if start_num > max_n or end_num > max_n:
+            print('本站最多只有%d页,请重新输入' %max_n)
             continue
         elif end_num<start_num:
-            print ('你开玩笑的吧。。。。')
-            new_num=end_num
-            end_num=start_num
-            start_num=end_num
+            print('你开玩笑的吧。。。。')
+            new_num = end_num
+            end_num = start_num
+            start_num = end_num
             return (start_num,end_num)
         else:
             return (start_num,end_num)
 
-if __name__=='__main__':
-    if os.path.exists('img')==False:
+if __name__ == '__main__':
+    if os.path.exists('img') == False:
         os.mkdir('img')
     while True:
-        choose=input('请选择想要抓取的图片网站1.糗事热图 2.煎蛋无聊图 3.退出\n').strip()
-        if choose=='1':
-            QS=QiuShi('https://www.qiushibaike.com/imgrank/' , 'page/')
+        choose = input('请选择想要抓取的图片网站1.糗事热图 2.煎蛋无聊图 3.退出\n').strip()
+        if choose == '1':
+            QS = QiuShi('https://www.qiushibaike.com/imgrank/' , 'page/')
             totalpage = QS.open_web()
-            print ('本站热图总共有%d页' %totalpage)
-            s1,e1=correct_input(totalpage)
-            tt1 = threading.Thread(target=QS.save_address, args=(s1,e1))
-            tt1.start()
-            tt1.join()
-        elif choose=='2':
-            JD=JianDan('http://jandan.net/ooxx/','page','-','#comments')
+            print('本站热图总共有%d页' %totalpage)
+            s1, e1 = correct_input(totalpage)
+            ts1 = []
+            for i in range(s1, e1+1):
+                t = threading.Thread(target=QS.open_address,args=(i,))
+                t.start()
+                ts1.append(t)
+            for t in ts1:
+                t.join()
+        elif choose == '2':
+            JD = JianDan('http://jandan.net/ooxx/', 'page', '-', '#comments')
             totalpage = JD.open_web()
-            print ('本站无聊图一共有%d页' %totalpage)
-            s2,e2 = correct_input(totalpage)
-            tt2 = threading.Thread(target=JD.save_address, args=(s2,e2))
-            tt2.start()
-            tt2.join()
+            print('本站无聊图一共有%d页' %totalpage)
+            s2, e2 = correct_input(totalpage)
+            ts2 = []
+            for i in range(s2,e2+1):
+                t = threading.Thread(target=JD.open_address, args=(i,))
+                t.start()
+                ts2.append(t)
+            for t in ts2:
+                t.join()
         else:
-            print ('程序退出')
+            print('程序退出')
             break
